eb97cf5197b34830d2858a934dc3448500832114
moamen <mmibrahem76@gmail.com>
2018-09-24 23:23:01 +0300

    second commit

--- logistic Regression (numpy)/logisticRegression.py
+++ logistic Regression (numpy)/logisticRegression.py
@@ -9,6 +9,29 @@ def sigmoid(x):
     expon=1+np.exp(-x)
     return 1/expon
 
+def L1(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    Returns:
+    loss -- the value of the L1 loss function defined above
+    """
+    loss = np.sum(np.abs(y-yhat))
+    return loss
+
+def L2(yhat, y):
+    """
+    Arguments:
+    yhat -- vector of size m (predicted labels)
+    y -- vector of size m (true labels)
+    
+    Returns:
+    loss -- the value of the L2 loss function defined above
+    """
+    loss = np.sum(np.dot(y-yhat,y-yhat))    
+    return loss
+
 for iter in range(m):
     ### NO way to get rid of this for loop 
     z=np.dot(w.T,x)+b 
