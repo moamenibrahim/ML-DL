9dcb45f4d33468a504c6d65bf4f270a669e24c33
moamen <mmibrahem76@gmail.com>
2018-10-01 21:28:54 +0300

    part 2 finished

---
+++ sickit-learn/logistic_regression.py
@@ -0,0 +1,47 @@
+from sklearn.model_selection import train_test_split
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import pandas as pd
+
+boston=pd.read_csv('boston.csv')
+print(boston.head())
+X= boston.drop('MEDV',axis=1).values
+y= boston['MEDV'].values
+X_rooms=X[:,5]
+type(X_rooms),type(y)
+y=y.reshape(-1,1)
+X_rooms=X_rooms.reshape(-1,1)
+plt.scatter(X_rooms,y)
+plt.ylabel('Value of house /1000 ($)')
+plt.xlabel('Number of rooms')
+plt.show()
+
+
+#### Fitting regression model 
+import numpy as np 
+from sklearn import linear_model
+
+reg=linear_model.LinearRegression()
+reg.fit(X_rooms,y)
+prediction_space=np.linspace(min(X_rooms),max(X_rooms)).reshape(-1,1)
+plt.scatter(X_rooms,y,color='blue')
+plt.plot(prediction_space,reg.predict(prediction_space),color='black',linewidth=3)
+plt.show()
+
+
+# Import LinearRegression
+from sklearn.linear_model import LinearRegression
+# Create the regressor: reg
+reg = LinearRegression()
+# Create the prediction space
+prediction_space = np.linspace(min(X_fertility), max(X_fertility)).reshape(-1,1)
+# Fit the model to the data
+reg.fit(X_fertility, y)
+# Compute predictions over the prediction space: y_pred
+y_pred = reg.predict(prediction_space)
+# Print R^2 
+print(reg.score(X_fertility, y))
+# Plot regression line
+plt.plot(prediction_space, y_pred, color='black', linewidth=3)
+plt.show()
+
