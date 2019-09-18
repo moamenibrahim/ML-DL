974733a47f3966be364764f44024d28c2ec5caf1
Moamen Ibrahim <mmibrahem76@gmail.com>
2019-09-18 14:44:39 +0300

    Fix matplotlib in NN

--- neural_network/planar_nn.py
+++ neural_network/planar_nn.py
@@ -1,20 +1,21 @@
 # Package imports
-import numpy as np
-import matplotlib.pyplot as plt
-from tests.testCases import *
-import sklearn
-import sklearn.datasets
 import sklearn.linear_model
+import sklearn.datasets
+import sklearn
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+sys.path.append('..')
 from utils.planar_utils import plot_decision_boundary, sigmoid, load_planar_dataset, load_extra_datasets
-
-# %matplotlib inline
+from tests.testCases import *
 
 np.random.seed(1)  # set a seed so that the results are consistent
 
 X, Y = load_planar_dataset()
 
 # Visualize the data:
-plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral)
+plt.scatter(X[0, :], X[1, :], c=Y.ravel(), s=40, cmap=plt.cm.Spectral)
+plt.show()
 
 # START CODE HERE ### (≈ 3 lines of code)
 shape_X = X.shape
@@ -457,4 +458,5 @@ if dataset == "blobs":
     Y = Y % 2
 
 # Visualize the data
-plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral)
+plt.scatter(X[0, :], X[1, :], c=Y.ravel(), s=40, cmap=plt.cm.Spectral)
+plt.show()
